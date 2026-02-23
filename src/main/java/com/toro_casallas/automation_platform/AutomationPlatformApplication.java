22bc814ff33aaf745ed05e61241894c75eb07b40
Torito9609 <toroandres85@gmail.com>
2026-02-23 13:10:09 -0500

    initial commit

---
+++ src/main/java/com/toro_casallas/automation_platform/AutomationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.toro_casallas.automation_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toro_casallas/automation_platform/AutomationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toro_casallas.automation_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
